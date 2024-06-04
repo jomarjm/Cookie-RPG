5b1dbe025e024539b27b5145970ccacefbfb6c06
jomarjm <71677200+jomarjm@users.noreply.github.com>
2024-06-04 11:09:07 -0400

    more game functionalities. bar dialogue and locations.

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ const mainEl = document.querySelector("main");
 // game variables init
 let health = 100;
 let xp = 0;
+let gold = 0;
 let initialStatPoints = 10;
 let attack = 0;
 let defense = 0;
@@ -26,6 +27,9 @@ let speed = 0;
 let weaponsInventory = [];
 let armorInventory = [];
 let playerName = "";
+let level = 1;
+let maxHealth = 100;
+let maxXP = 100;
 
 // inventory options data structures
 const weapons = [
@@ -83,13 +87,12 @@ const armors = [
 
 ]
 
-/*
 const locations = [
     {
         name: "Welcome Screen",
         buttonText: ["Continue", "Continue", "Continue"],
         buttonFunctions: [toBackground1, toBackground1, toBackground1],
-        text: `Welcome, <span class="bold">${playerName}</span>...`
+        text: "" // `Welcome, <span class="bold">${playerName}</span>...`
     },
     {
         name: "Background 1",
@@ -112,18 +115,63 @@ const locations = [
     {
         name: "Background 4",
         buttonText: ["Continue", "Continue", "Continue"],
-        buttonFunctions: [toMainMenu, toMainMenu, toMainMenu],
+        buttonFunctions: [toTownSquare, toTownSquare, toTownSquare],
         text: "But you don't have any money. See what you can do to survive."
     },
     {
-        name: "Main Menu",
+        name: "Town Square",
         buttonText: ["North Market", "South Market", "Leave Town"],
         buttonFunctions: [toNorthMarket, toSouthMarket, leaveTown],
-        text: "What will you do?"
-    }
+        text: "There's a road to the North Market and a road to the South Market. " +
+              "The town gates are also here. What will you do?"
+    },
+    {
+        name: "North Market",
+        buttonText: ["Weapon Shop", "Armor Shop", "Town Square"],
+        buttonFunctions: [toNorthMarket, toSouthMarket, toTownSquare],
+        text: "There's a weapon shop and an armor shop in this part of town. What will you do?"
+    },
+    {
+        name: "South Market",
+        buttonText: ["Oven Inn", "Bar", "Town Square"],
+        buttonFunctions: [toNorthMarket, toBar, toTownSquare],
+        text: "There's an inn and a bar in this part of town. What will you do?"
+    },
+    {
+        name: "The Fields",
+        buttonText: ["Cookie Castle", "Fight Monsters", "Town Square"],
+        buttonFunctions: [], // [toCookieCastle, fightMonsters, toTownSquare],
+        text: "Ahhh... The Fields... Where the evil Dark Cookie Lord's evil cookie eating minions roam free. " +
+              "The road to the Dark Lord's castle can be seen in the distance. What will you do?"
+    },
+    {
+        name: "The Bar",
+        buttonText: ["Get a Drink", "Talk to People", "Town Square"],
+        buttonFunctions: [getDrink, talkToPeople, toTownSquare],
+        text: "The Bar. Where all strange homies hang out."
+    },
     
+];
+
+const barHomiePhrases = [
+    "I came to this town riding on an ass... YO MAMA'S ASS!",
+    "This town smells like ass... Nothing you'll find me complainging about though!",
+    "Is that...?! ...oooooOOOOOOOOOOOOOOOOooooooooooohhhh!",
+    "OOOOOOOOOOOOOOOOOOO",
+    "You know what? I love myself. Even though I look like a BURNT chicken nugget, I still love myself!",
+    "What battle scars? Oh, these! Yeah, no, those are bite marks. My wif-, I mean, my dog, yes, my dog! " +
+    "Yeah, he bites real hard sometimes...",
+    "One time I stuck my entire foot in my mouth",
+    "Ain't none of these fuckos old enough to go to this damn club!",
+    "When I came home drunk late last night, my wife asked me if I could tell the time. " +
+    "I then turned around around to the clock and said, 'I'M NOT FUCKING DRUNK!'",
+    "Casio keyboards are MUCH, MUCH easier to play than Yamahas, Nords, all those fancy ass keyboards.",
+    "I always tell people in bands that they will make more money if they buy IEMs from me.",
+    "I think... I may have autism...",
+    "Fuck it... FORZA!",
+    "Fuck it... TEKKEN!",
+    "Fuck it... ANIMAL CROSSING!",
 ]
-*/
 
 // set button variables
 const startBtn = document.getElementById("start-game-btn");
@@ -135,6 +183,17 @@ const setNameBtn = document.getElementById("set-name-btn");
 function startGame() {
     gameTitleHeader.style.display = "none";
     mainEl.style.display = "block";
+}
+
+function levelUp() {
+    xp = 0;
+    level++;
+    attack += Math.floor(Math.random() * 3);
+    defense += Math.floor(Math.random() * 3);
+    speed += Math.floor(Math.random() * 3);
+    maxHealth += Math.floor(Math.random() * 15);
+    maxXP += 20;
+    // add text display
 }
 
 function setName() {
@@ -187,7 +246,9 @@ function setStats() {
         // console.log(attack, defense, speed);
         introSection.style.display = "none";
         gameContainer.style.display = "flex";
-        gameStart();
+        update(locations[0]);
+        // console.log(playerName);
+        textBox.innerHTML = `Welcome, <span class="bold">${playerName}</span>...`;
     }
 }
 
@@ -214,17 +275,81 @@ function resetTypeWriter() {
 */
 
 function update(location) {
-
+    btn1.innerText = location.buttonText[0];
+    btn2.innerText = location.buttonText[1];
+    btn3.innerText = location.buttonText[2];
+    btn1.onclick = location.buttonFunctions[0];
+    btn2.onclick = location.buttonFunctions[1];
+    btn3.onclick = location.buttonFunctions[2];
+    textBox.innerHTML = location.text;
 }
 
+function toBackground1() {
+    update(locations[1]);
+}
+
+function toBackground2() {
+    update(locations[2]);
+}
+
+function toBackground3() {
+    update(locations[3]);
+}
+
+function toBackground4() {
+    update(locations[4]);
+}
+
+function toTownSquare() {
+    update(locations[5]);
+}
+
+function toNorthMarket() {
+    update(locations[6]);
+}
+
+function toSouthMarket() {
+    update(locations[7]);
+}
+
+function leaveTown() {
+    update(locations[8]);
+}
+
+function toBar() {
+    update(locations[9]);
+}
+
+
+
+function talkToPeople() {
+    textBox.innerHTML = barHomiePhrases[Math.floor(Math.random() * barHomiePhrases.length)];
+}
+
+function getDrink() {
+    if (money >= 10) {
+        money -= 10;
+        // add money display here
+        health += 20;
+        // add health display change
+    } else {
+        textBox.innerHTML = "You haven't got the mons for a drink right now.";
+    }
+}
+
+/*
 function gameStart() {
-    textBox.innerHTML = `Welcome, <span class="bold">${playerName}</span>...`;
+    // textBox.innerHTML = `Welcome, <span class="bold">${playerName}</span>...`;
+    update(locations[0]);
 }
+*/
 
+/*
 function backgroundStory() {
     setTimeout(resetTypeWriter, 3000);
     
 }
+*/
 
 // initialize buttons
 startBtn.addEventListener("click", startGame);
